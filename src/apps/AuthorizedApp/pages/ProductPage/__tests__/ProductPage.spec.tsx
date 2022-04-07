dfb17b83898af386ffb41b651a297126c4421c38
Bartosz Szafran <bartosz.szafran@practi.net>
2022-04-07 19:44:48 +0200

    Basket - test for the scenario with two products added to the basket

--- src/apps/AuthorizedApp/pages/ProductPage/__tests__/ProductPage.spec.tsx
+++ src/apps/AuthorizedApp/pages/ProductPage/__tests__/ProductPage.spec.tsx
@@ -88,5 +88,61 @@ describe("ProductPage component", () => {
         within(purchaseCard).getByText(mockedFirstProduct.name)
       })
     })
+
+    describe("when two products are added to the basket", () => {
+      it("displays the products and calculates basket balance", async () => {
+        render(withWrappers(<ProductPage />))
+
+        const mockedFirstProduct = mockedProducts[0]
+        const mockedSecondProduct = mockedProducts[1]
+
+        {
+          // Select item on the grid - first product
+          const purchaseItemsSection = await screen.findByLabelText(
+            "Purchase items"
+          )
+          const firstProduct = await within(purchaseItemsSection).findByText(
+            mockedFirstProduct.name
+          )
+          userEvent.click(firstProduct)
+
+          // Add item to the basket
+          const addButton = await screen.findByText(
+            `ADD ₿ ${mockedFirstProduct.price.toFixed(2)}`
+          )
+          userEvent.click(addButton)
+        }
+
+        {
+          // Select item on the grid - second product
+          const purchaseItemsSection = await screen.findByLabelText(
+            "Purchase items"
+          )
+          const secondProduct = await within(purchaseItemsSection).findByText(
+            mockedSecondProduct.name
+          )
+          userEvent.click(secondProduct)
+
+          // Add item to the basket
+          const addButton = await screen.findByText(
+            `ADD ₿ ${mockedSecondProduct.price.toFixed(2)}`
+          )
+          userEvent.click(addButton)
+        }
+
+        // Check if the item is displayed in the basket
+        const purchaseCard = await screen.findByLabelText("Purchase card")
+
+        const expectedBasketBalance =
+          mockedFirstProduct.price + mockedSecondProduct.price
+        // balance
+        expect(
+          within(purchaseCard).getByLabelText("Basket balance")
+        ).toHaveTextContent(`₿ ${expectedBasketBalance.toFixed(2)}`)
+        // card
+        within(purchaseCard).getByText(mockedFirstProduct.name)
+        within(purchaseCard).getByText(mockedSecondProduct.name)
+      })
+    })
   })
 })
